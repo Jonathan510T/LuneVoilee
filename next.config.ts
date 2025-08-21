08887913b29f68c7997d50dace6619bbacea2274
Jonathan510T <jonathant0842@gmail.com>
2025-08-20 22:43:40 -0400

    edits to next.config

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,20 @@
 // next.config.js
 /** @type {import('next').NextConfig} */
+
+const repo = 'LuneVoilee'; 
+
 const nextConfig = {
-  output: 'export', // <-- THIS enables `next export`
+  output: 'export', 
+
+
+  basePath: `/${repo}`,
+  assetPrefix: `/${repo}/`,
+
   images: {
-    unoptimized: true, // Required if you use next/image in static export
+    unoptimized: true, 
   },
-  trailingSlash: true, // Optional: helps ensure all links resolve
+
+  trailingSlash: true, 
 };
 
 module.exports = nextConfig;
